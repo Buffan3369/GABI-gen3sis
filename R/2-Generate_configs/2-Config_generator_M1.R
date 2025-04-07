506c16e82a14959b3835e846df924bcec2b32e0d
Lucas Buffan <94531347+Buffan3369@users.noreply.github.com>
2025-04-07 18:41:16 +0200

    model update

--- R/2-Generate_configs/2-Config_generator_M1.R
+++ R/2-Generate_configs/2-Config_generator_M1.R
@@ -1,5 +1,5 @@
 ################################################################################
-# Goal: Generate configs for the M0 scenario (fully-connected landscape, 
+# Goal: Generate configs for the M1 scenario (fully-connected downscaled landscape, 
 #   species distribution, ecology and evolution only mediated by climated 
 #   constraints, i.e. prec + temp)
 ################################################################################
@@ -21,19 +21,17 @@ linMap <- function(x, from, to, rnd = F, dgts) {
 }
 
 for(start_region in c("North_America", "South_America")){
-## Initialise 8-dim Sobol' sequence --------------------------------------------
-  param_df <- data.frame(cbind(paste0("Simulation_", 1:n), sobol(n = n, dim = 8)))
+  ## Initialise 5-dim Sobol' sequence --------------------------------------------
+  param_df <- data.frame(cbind(paste0("Simulation_", 1:n), sobol(n = n, dim = 6)))
   colnames(param_df) <- c("Sim_index",  # Simulation index
                           "S",          # Divergence threshold above which speciation occurs
                           "omega_T",    # Strength of environmental filtering for temperature
                           "omega_P",    # Same for precipitations
                           "sigma_e",    # Rate of environmental niche evolution
                           "disp_shape", # Shape of the dispersal kernel (Weibull distribution)
-                          "disp_scale", # Scale of the dispersal kernel
-                          "decay",      # Decay parameter for extirpation (i.e., local extinction) probability
-                          "inflexion")  # Inflexion parameter for extirpation probability
+                          "disp_scale") # Scale of the dispersal kernel
   
-## Rescale parameters to desired range -----------------------------------------
+  ## Rescale parameters to desired range -----------------------------------------
   
   ## INSÉRER LES RANGES EN SUP MAT ##
   
@@ -43,37 +41,38 @@ for(start_region in c("North_America", "South_America")){
   param_df$sigma_e <-    linMap(as.numeric(param_df$sigma_e), from = 0.005, to = 0.15, rnd = T, dgts = 3)
   param_df$disp_shape <- linMap(as.numeric(param_df$disp_shape), from = 0.5, to = 3, rnd = T, dgts = 3)
   param_df$disp_scale <- linMap(as.numeric(param_df$disp_scale), from = 0.1, to = 1, rnd = T, dgts = 3)
-  param_df$decay <-      linMap(as.numeric(param_df$decay), from = -4, to = 1, rnd = T, dgts = 3)
-  param_df$inflexion <- linMap(as.numeric(param_df$inflexion), from = 500, to = 1400, rnd = T, dgts = 3) # OM of individual richness, can't go below
   
-## Add fixed parameters --------------------------------------------------------
+  ## Add fixed parameters --------------------------------------------------------
   param_df$seed <- 1:n                # random seed, for replicate reproducibility
+  param_df$divergence_factor <- 0.05  # how much divergence species accumulate across iterations
   param_df$max_species <- 12000       # max number of species (above which simulation aborpts)
   param_df$max_coex_sp <- 1000        # max nb. of species per cell
   param_df$start_species <- 1         # number of species to start with
   param_df$init_ab <- 500             # initial abundance of starting species
   param_df$grid_cell_distance <- 111  # grid cell distance (1x1° lat in km at the equator)
-
-## Save parameter table --------------------------------------------------------
+  
+  ## Save parameter table --------------------------------------------------------
+  if(!dir.exists("./Data/Gen3sis_parameter_tables/M1")){dir.create("./Data/Gen3sis_parameter_tables/M1")}
   write.tbl.std(param_df, 
-                  paste0("./Data/Gen3sis_parameter_tables/M0/", start_region, "_parameters.txt"))
-## Generate config files -------------------------------------------------------
+                paste0("./Data/Gen3sis_parameter_tables/M1/", start_region, "_parameters.txt"))
+  ## Generate config files -----------------------------------------------------
+  out <- paste0("./Data/Gen3sis_configs/M1/", start_region, "_start/")
+  if(!file.exists(out)){dir.create(out)}
   for(i in 1:n){
     # Select settings for simulation i
     params <- param_df[i, ]
     # Create directory where config will be stored
-    out_dir <- paste0("./Results/M0/", start_region, "_start/Config_", i)
+    out_dir <- paste0(out, "/Config_", i)
     dir.create(out_dir)
     # Edit config template
-    config_i <- readLines(paste0("./Results/M0/config_M0_", start_region, "_template.R"))
+    config_i <- readLines(paste0("./Data/Gen3sis_configs/M1/config_M1_", start_region, "_template.R"))
     config_i <- gsub("params\\$S", params$S, config_i)
+    config_i <- gsub("params\\$divergence_factor", params$divergence_factor, config_i)
     config_i <- gsub("params\\$omega_T", params$omega_T, config_i)
     config_i <- gsub("params\\$omega_P", params$omega_P, config_i)
     config_i <- gsub("params\\$sigma_e", params$sigma_e, config_i)
     config_i <- gsub("params\\$disp_shape", params$disp_shape, config_i)
     config_i <- gsub("params\\$disp_scale", params$disp_scale, config_i)
-    config_i <- gsub("params\\$decay", params$decay, config_i)
-    config_i <- gsub("params\\$inflexion", params$inflexion, config_i)
     config_i <- gsub("params\\$seed", params$seed, config_i)
     config_i <- gsub("params\\$max_species", params$max_species, config_i)
     config_i <- gsub("params\\$max_coex_sp", params$max_coex_sp, config_i)
@@ -81,6 +80,6 @@ for(start_region in c("North_America", "South_America")){
     config_i <- gsub("params\\$init_ab", params$init_ab, config_i)
     config_i <- gsub("params\\$grid_cell_distance", params$grid_cell_distance, config_i)
     # Write config for run i
-    writeLines(config_i, paste0(out_dir, "/config_M0_", start_region, "_run_", i, ".R"))
+    writeLines(config_i, paste0(out_dir, "/config_M1_", start_region, "_run_", i, ".R"))
   }  
 }
--- R/3-Config_runner.R
+++ R/3-Config_runner.R
@@ -7,14 +7,16 @@
 library(gen3sis)
 library(future.apply)
 
+args <- commandArgs(trailingOnly=TRUE)
+
 ## Function to run a config file by its index ---------------------------------- 
 run_sim_i <- function(i, where = c("North", "South")){
   # Path towards the i-th config file
-  config_dir <- paste0("../Outputs/M0/", where, "_America_start/Config_", i)
+  config_dir <- paste0("../Data/Gen3sis_configs/", args[1], "/", where, "_America_start/Config_", i, "/config_M1_", where, "_America_run_", i, ".R")
   # Path towards landscape
-  input_dir <- "Data/Landscapes/Fully_connected"
+  input_dir <- "../Data/Landscapes/downscaled_landscape_10/"
   # Path towards output directory
-  output_dir <- paste0("../Outputs/M0/", where, "_America_start/Config_", i)
+  output_dir <- paste0("../Outputs/", args[1], "/", where, "_America_start/")
   # Run simulation
   sim <- run_simulation(config = config_dir,
                         landscape = input_dir,
@@ -22,7 +24,7 @@ run_sim_i <- function(i, where = c("North", "South")){
 }
 
 ## Set-up multithreading and apply the function --------------------------------
-plan(multicore, workers = 33)
-future_sapply(X = 1:100, FUN = run_sim_i, where = "North") # Ancestor from North America
-future_sapply(X = 1:100, FUN = run_sim_i, where = "South") # Ancestor from South America
+plan(multicore, workers = 40)
+future_sapply(X = 1:500, FUN = run_sim_i, where = "North") # Ancestor from North America
+future_sapply(X = 1:500, FUN = run_sim_i, where = "South") # Ancestor from South America
 plan(sequential)
