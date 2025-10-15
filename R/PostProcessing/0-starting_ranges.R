0d11a63c5f2f3be2f6e447457db6c65a9797c719
Lucas Buffan <94531347+Buffan3369@users.noreply.github.com>
2025-10-15 18:25:37 +0200

    same as previous commit

--- R/PostProcessing/0-starting_ranges.R
+++ R/PostProcessing/0-starting_ranges.R
@@ -8,23 +8,24 @@ library(raster)
 
 args <- commandArgs(trailingOnly = T) # don't specify mdl as starting ranges are shared
 from <- args[1]   # North or South
-nruns <- args[2]  # 500
+model <- args[2]
+nruns <- args[3]  # 500
 
 ## Unstandardised --------------------------------------------------------------
 # Initialise
 run <- 1
-path_to_pa <- paste0("../Outputs/M0/", from, "_America_start/config_M0_", from, "_America_run_", run, "/pa_matrices/")
+path_to_pa <- paste0("../Outputs/", model, "/", from, "_America_start/config_", model, "_", from, "_America_run_", run, "/pa_matrices/")
 pa_0 <- readRDS(paste0(path_to_pa, "pa_t_499.rds")) # oldest time step numbered last in gen3sis
 
 # Loop across remaining runs
 for(run in 2:nruns){
-  path_to_pa <- paste0("../Outputs/M0/", from, "_America_start/config_M0_", from, "_America_run_", run, "/pa_matrices/")
-  m0 <- readRDS(paste0(path_to_pa, "pa_t_499.rds"))
-  pa_0[which(m0[,3] == 1),3] <- run
+  path_to_pa <- paste0("../Outputs/", model, "/", from, "_America_start/config_", model, "_", from, "_America_run_", run, "/pa_matrices/")
+  mat <- readRDS(paste0(path_to_pa, "pa_t_499.rds"))
+  pa_0[which(mat[,3] == 1),3] <- run
 }
 
 # Save xyz tbl
-saveRDS(pa_0, paste0("../Outputs/all_starting_ranges/", from, "_America_start_xyz.RDS"))
+saveRDS(pa_0, paste0("../Outputs/all_starting_ranges/", model, "_", from, "_America_start_xyz.RDS"))
 
 # Rasterise, plot and save
 # r0 <- rasterFromXYZ(pa_0)
