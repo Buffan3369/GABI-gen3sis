54752dda7d1e17763c806ba3381fe7c5486c1ac5
Lucas Buffan <94531347+Buffan3369@users.noreply.github.com>
2025-10-15 10:15:23 +0200

    adapt fig. 4b code

--- R/Visualisation/Main/Figure_biomes.R
+++ R/Visualisation/Main/Figure_biomes.R
@@ -9,7 +9,7 @@ library(raster)
 library(sf)
 library(tidyverse)
 
-source("./R/useful/helper_functions.R")
+source("./R/PostProcessing/4-Extract_distribs_with_moments.R")
 
 ################################################################################
 ###################### 1. BROADCLASS BIOME MAP AT t=5Ma ########################
@@ -52,31 +52,39 @@ ggsave("./Figures/MS/Main/Figure_biomes/biome_map.pdf", plot = biome_5M_plot, he
 ################ 2. PROPORTION OF REALISED EXCHANGE PER BIOME ##################
 ################################################################################
 
+P_tbl_all <- summarise_distrib(metric = "start_biome",
+                               what = "all")
+
+P_tbl_exchanged <- summarise_distrib(metric = "start_biome",
+                                     what = "exchanged")
+
 # Nb of starting species per biome
-per_biome_df <- P_tbl %>% 
+per_biome_df <- P_tbl_all %>% 
   filter(!(is.na(start_biome))) %>% 
-  count(start_biome, model, start_region)
+  mutate(start = sapply(X = start, FUN = function(x){paste0(x, " America")})) %>% 
+  count(start_biome, model, start)
 # Same but only for exchanged species
-per_biome_exch_df <- P_tbl %>% 
-  filter(!(is.na(start_biome)) & exchanged == 1) %>% 
-  count(start_biome, model, start_region)
+per_biome_exch_df <- P_tbl_exchanged %>% 
+  mutate(start = sapply(X = start, FUN = function(x){paste0(x, " America")})) %>% 
+  count(start_biome, model, start)
 ## No polar species exchanged from NA in M0, no cold species exchanged from SA in M0: add them
 per_biome_exch_df <- per_biome_exch_df %>% 
-  add_row(start_biome = "5", model = "M0", start_region = "North America", n = 0, .before = 32) %>% 
-  add_row(start_biome = "4", model = "M0", start_region = "South America", n = 0, .before = 26)
+  add_row(start_biome = 5, model = "M0", start = "North America", n = 0, .before = 32) %>% 
+  add_row(start_biome = 4, model = "M0", start = "South America", n = 0, .before = 26)
 # Assess proportion
 per_biome_exch_df <- per_biome_exch_df %>% 
   mutate(n_init = per_biome_df$n) %>% 
   mutate(prop_exch = n / per_biome_df$n)
 # Plot
 prop_biome_success_plot <- per_biome_exch_df %>% 
+  mutate(start_biome = as.character(start_biome)) %>% 
   ggplot(aes(x = start_biome, y = prop_exch)) +
   geom_col(aes(fill = start_biome), linewidth = 0.3, colour = "black") +
   geom_text(aes(label = sapply(prop_exch, round, digits = 2)), inherit.aes = TRUE, nudge_y = 0.05, size = 2.5) +
   scale_fill_manual(values = c("1" = "#f9d14a", "2" = "#ab3329", "3" = "#ed968c", "4" = "#7c4b73", "5" = "#88a0dc")) +
   scale_x_discrete(labels = c("1" = "Tropical", "2" = "Arid", "3" = "Temperate", "4" = "Cold", "5" = "Polar")) +
   labs(x = "Ancestral biome", y = "Prop. success") +
-  facet_grid(model~start_region) +
+  facet_grid(model~start) +
   theme(axis.text = element_text(size = 7),
         axis.title = element_text(size = 10),
         axis.line = element_line(linewidth = 0.3, color = "black"),
