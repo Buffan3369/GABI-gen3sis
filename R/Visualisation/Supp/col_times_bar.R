2afcb6106b82f60f2a8b812ff24943f8c1698021
Lucas Buffan <94531347+Buffan3369@users.noreply.github.com>
2025-11-04 18:07:55 +0100

    plotting colonisation timings

--- R/Visualisation/Supp/col_times_bar.R
+++ R/Visualisation/Supp/col_times_bar.R
@@ -22,42 +22,20 @@ for(mdl in c("M0", "M1")){
   }
 }
 rownames(time_df) <- 1:nrow(time_df)
-# Temperature data
-av_clim <- readRDS("./Data/PALEO_PGEM-bioclim/average_clim_both_continents.RDS")
-temp <- av_clim %>%
-  filter(var == "Temperature")
-
-# Create the temperature column
-choose_temp <- function(i){
-  t <- time_df$time[i]*10
-  cont <- strsplit(time_df$start[i], split = " ")[[1]][1]
-  corr_T <- temp$clim_av[which(temp$time == t & temp$continent == cont)]
-  return(corr_T)
-}
-
-Temperature <- sapply(X = 1:nrow(time_df), FUN = choose_temp)
-time_df$Temperature <- as.numeric(Temperature)
-
-M <- mean(time_df$Temperature)
-SD <- sd(time_df$Temperature)
-time_df$std_temperature <- sapply(time_df$Temperature,
-                                  FUN = function(x){
-                                    return((x-M)/SD)
-                                  })
 
 # Plot
-time_df %>% 
+time_hist <- time_df %>% 
   ggplot(aes(x = time)) +
   scale_x_reverse(breaks = seq(500,0,-100), labels = seq(5,0,-1)) +
-#  geom_histogram(binwidth = 10, aes(y = after_stat(density)), fill = "#FAFFF0", colour = "black") +
-  geom_line(aes(y = Temperature), linewidth = 1) +
+  geom_histogram(binwidth = 10, aes(y = after_stat(density)), fill = "#FAFFF0", colour = "black", alpha = 0.5) +
+#  geom_line(aes(y = Temperature), linewidth = 1) +
 #  geom_density(bw = 10, linewidth = 1) +
-  labs(x = "Time (Ma)") +
+  labs(x = "Time (Ma)", y = "Exchange density") +
 #  scale_y_continuous(sec.axis = sec_axis(~ . * 100)) +
 #  ylim(c(0, 0.011)) +
-  facet_grid(model~start) +
-  theme(axis.title = element_text(size = 7.5),
-        axis.text = element_text(size = 5),
+  facet_wrap(model~start, scales = "free_y") +
+  theme(axis.title = element_text(size = 9),
+        axis.text = element_text(size = 7),
         axis.line = element_line(linewidth = 0.3, color = "black"),
         legend.position = "none",
         panel.background = element_rect(fill = "#F5F0FF", colour = "black"),
@@ -65,6 +43,46 @@ time_df %>%
         panel.grid.minor = element_line(linewidth = 0.1, color = "grey50"),
         plot.margin = unit(c(0.5,0.5,0.5,0.5), "cm"),
         strip.background = element_rect(fill = "#DDE6F5")) +
-  coord_geo(dat = epochs, abbrv = F, height = unit(1, "line"))
-  
+  coord_geo(dat = epochs, abbrv = F, height = unit(1, "line"), size = 2.5, center_end_labels = T)
 
+ggsave("./Figures/MS/Supp/colonisation_timing/time_hist.pdf", plot = time_hist,
+       height = 150, width = 200, units = "mm")
+
+# Temperature only
+
+av_clim <- readRDS("./Data/PALEO_PGEM-bioclim/average_clim_both_continents.RDS")
+temp <- av_clim %>%
+  filter(var == "Temperature")
+
+temp1 <- temp
+temp1$model <- "M1"
+temp$model <- "M0"
+temp <- rbind.data.frame(temp, temp1)
+temp <- temp %>% 
+  filter(time %in% seq(0, 5000, 10)) %>% 
+  mutate(time = time / 1000)
+
+temp_panel <- temp %>% 
+  ggplot(aes(x = time)) +
+  scale_x_reverse() +
+  scale_y_continuous(position = "right") +
+  geom_line(aes(y = clim_av), linewidth = 0.25, colour = "#993404") +
+  labs(x = "Time (Ma)", y = "Temperature (°C)") +
+  facet_wrap(model~continent, scales = "free_y") +
+  theme(axis.title.x = element_text(size = 9),
+        axis.title.y = element_text(size = 9, colour = "#993404"),
+        axis.text.x = element_text(size = 7),
+        axis.text.y = element_text(size = 7, colour = "#993404"),
+        axis.line.x = element_line(linewidth = 0.3, color = "black"),
+        axis.line.y = element_line(linewidth = 0.3, color = "#993404"),
+        axis.ticks.y = element_line(colour = "#993404"), 
+        legend.position = "none",
+        panel.background = element_rect(fill = "#F5F0FF", colour = "black"),
+        panel.grid.major = element_line(linewidth = 0.1, color = "grey50"),
+        panel.grid.minor = element_line(linewidth = 0.1, color = "grey50"),
+        plot.margin = unit(c(0.5,0.5,0.5,0.5), "cm"),
+        strip.background = element_rect(fill = "#DDE6F5")) +
+  coord_geo(dat = "epochs", abbrv = F, height = unit(1, "line"), size = 2.5, center_end_labels = T)
+
+ggsave("./Figures/MS/Supp/colonisation_timing/temperature_panel.pdf", 
+       plot = temp_panel, height = 150, width = 200, units = "mm")
