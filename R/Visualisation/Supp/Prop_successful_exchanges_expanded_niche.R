3f5f00c30940e09c3deffbb8c030e0cf1b75fb0b
Lucas Buffan <94531347+Buffan3369@users.noreply.github.com>
2026-04-21 18:16:23 +0200

    plot success proportion with different w

--- R/Visualisation/Supp/Prop_successful_exchanges_expanded_niche.R
+++ R/Visualisation/Supp/Prop_successful_exchanges_expanded_niche.R
@@ -7,12 +7,27 @@
 ################################################################################
 
 library(tidyverse)
+library(ggpubr)
 
 source("./R/useful/helper_functions.R")
+
+################################################################################
+############################ PPGEM-based landscape #############################
+################################################################################
 
 ## Set up plotting dataset -----------------------------------------------------
 plot_df_prop_success <- readRDS("./Results/Exchanged_metrics/Prop_successful_exchange.RDS")
 plot_df_prop_success$w_max <- 0.1
+plot_df_prop_success <- plot_df_prop_success %>% filter(Model %in% c("M0", "M1"))
+
+crash <- readRDS("./Results/Exchanged_metrics/Crash_counter.RDS")
+crash <- crash %>%
+  select(Start, M0, M1) %>% 
+  mutate(w_max = 0.1) %>% 
+  filter(Start != "North America Standardised") %>% 
+  group_by(Start, w_max) %>% 
+  pivot_longer(cols = c(M0, M1), names_to = "model", values_to = "prop_crash") %>% 
+  ungroup()
 
 for(w in c(0.6, 1)){
   for(model in c("M0", "M1")){
@@ -21,35 +36,36 @@ for(w in c(0.6, 1)){
                               "/", model),
                        pattern = "North_America_parameters_EXTENDED_EXCH", full.names = T)[[1]]
     param_tbl <- read.table(path, header = T)
+    # store crash nb
+    n_crash <- length(which(param_tbl$exchanged == -1))
+    crash <- crash %>% add_row(Start = "North America", w_max = w,
+                               model = model, prop_crash = n_crash/500)
     # Process
     param_tbl <- param_tbl %>% filter(exchanged != -1)
-    p_success <- length(which(param_tbl$exchanged == 1)) / nrow(param_tbl)
+    n_success <- length(which(param_tbl$exchanged == 1))
+    p_success <- n_success / nrow(param_tbl)
     lwr_ci <- bino_CI(prop = p_success, n = nrow(param_tbl), what = "Lower")
     upr_ci <- bino_CI(prop = p_success, n = nrow(param_tbl), what = "Upper")
     # Extend plotting df
     plot_df_prop_success <- plot_df_prop_success %>% 
-      add_row(Ori = "North America", Prop_success = p_success, Lower_CI = lwr_ci,
-              Upper_CI = upr_ci, Model = model, w_max = w)
+      add_row(Ori = "North America", Nb_success = n_success, Prop_success = p_success, 
+              Lower_CI = lwr_ci, Upper_CI = upr_ci, Model = model, w_max = w)
   }
 }
 
-ptbl_NA_0.6_M0 <- read.table("./Data/Gen3sis_parameter_tables/Niche_expanded/w_0.6/M0/North_America_parameters_EXTENDED_EXCH_AREA_DIV_DIST.txt",
-                             header = T)
-ptbl_NA_0.6_M1 <- read.table("./Data/Gen3sis_parameter_tables/Niche_expanded/w_0.6/M1/North_America_parameters_EXTENDED_EXCH_AREA_DIV_DIST.txt",
-                             header = T)
+plot_df_prop_success <- plot_df_prop_success %>% 
+  mutate(cont_w = sapply(X = 1:nrow(plot_df_prop_success),
+                         FUN = function(x){paste0(plot_df_prop_success$Ori[x], 
+                                                  " \n(w_max=", plot_df_prop_success$w_max[x], ")")}))
 
-# w in [0.01, 1]
-
-
-prop_plot <- plot_df_prop_success %>% 
-  ggplot(aes(x = Ori, y = Prop_success)) +
+suc_plot <- plot_df_prop_success %>% 
+  ggplot(aes(x = cont_w, y = Prop_success)) +
   geom_errorbar(aes(ymin = Lower_CI, ymax = Upper_CI), width = 0.15) +
   geom_point(aes(fill = Ori), colour = "black", pch = 23, size = 1.5) +
   scale_fill_manual(values = c("#fb6a4aff", "#66c2a4ff")) +
   scale_y_continuous(expand = c(0, 0), limits = c(0, 1.12), breaks = c(0, 0.25, 0.5, 0.75, 1)) +
-  labs(x = NULL, y = NULL) +
   facet_wrap(.~Model, ncol = 2) +
-  labs(x = "Ancestral continent", y = "Proportion of successful exchange") +
+  labs(x = NULL, y = "Proportion of successful exchange") +
   theme(axis.title = element_text(size = 7.5),
         axis.text = element_text(size = 5),
         axis.line = element_line(linewidth = 0.3, color = "black"),
@@ -60,22 +76,17 @@ prop_plot <- plot_df_prop_success %>%
         plot.margin = unit(c(0.5,0.5,0.5,0.5), "cm"),
         strip.background = element_rect(fill = "#DDE6F5"))
 
-ggsave("./Figures/MS/Main/Figure_PropSuccess/Figure_PropSuccess.png", 
-       plot = prop_plot, height = 120, width = 110, dpi = 600, units = "mm")
-
-## Incorporating simulations that crashed --------------------------------------
-plot_df_prop_success_raw <- readRDS("./Results/Exchanged_metrics/RawProp_successful_exchange.RDS")
-
-prop_plot2 <- plot_df_prop_success_raw %>% 
-  filter(Model %in% c("M0", "M1")) %>% 
-  ggplot(aes(x = Ori, y = Prop_success)) +
-  geom_errorbar(aes(ymin = Lower_CI, ymax = Upper_CI), width = 0.15) +
-  geom_point(aes(fill = Ori), colour = "black", pch = 23, size = 1.5) +
+## Plot proportion of crashes
+crash <- crash %>% mutate(cont_w = sapply(X = 1:nrow(crash),
+                                          FUN = function(x){paste0(crash$Start[x], 
+                                                                   " \n(w_max=", crash$w_max[x], ")")}))
+crash_plot <- crash %>% 
+  ggplot(aes(x = cont_w, y = prop_crash)) +
+  geom_point(aes(fill = Start), colour = "black", pch = 21, size = 1.5) +
   scale_fill_manual(values = c("#fb6a4aff", "#66c2a4ff")) +
-  scale_y_continuous(expand = c(0, 0), limits = c(0, 1.12), breaks = c(0, 0.25, 0.5, 0.75, 1)) +
-  labs(x = NULL, y = NULL) +
-  facet_wrap(.~Model, ncol = 2) +
-  labs(x = "Ancestral continent", y = "Proportion of successful exchange") +
+  scale_y_continuous(expand = c(0, 0), limits = c(0, 0.65), breaks = c(0, 0.2, 0.4, 0.6)) +
+  facet_wrap(.~model, ncol = 2) +
+  labs(x = "Ancestral continent", y = "Proportion of crash") +
   theme(axis.title = element_text(size = 7.5),
         axis.text = element_text(size = 5),
         axis.line = element_line(linewidth = 0.3, color = "black"),
@@ -86,5 +97,12 @@ prop_plot2 <- plot_df_prop_success_raw %>%
         plot.margin = unit(c(0.5,0.5,0.5,0.5), "cm"),
         strip.background = element_rect(fill = "#DDE6F5"))
 
-ggsave("./Figures/MS/Supp/Figure_RawPropSuccessM0M1.png", 
-       plot = prop_plot2, height = 75, width = 120, dpi = 600, units = "mm")
+## Assemble and save
+com_pnl <- ggarrange(suc_plot, crash_plot, nrow = 2, labels = c("(A)", "(B)"))
+ggsave("./Figures/MS/Supp/niche_expanded/NA_vs_SA-PPGEM-diff_w.pdf", plot = com_pnl,
+       height = 150, width = 130, units = "mm")
+  
+crash <- crash %>% 
+  select(!cont_w)
+
+saveRDS(crash, "./Results/Exchanged_metrics/Crash_counter_diff_w.RDS")
